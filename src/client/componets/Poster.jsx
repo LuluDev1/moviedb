862378b8e8af6b52924eef60c8f5eb382217aef2
Lu <96496642+LuluDev1@users.noreply.github.com>
2024-06-13 01:49:43 -0500

    chore: Update dependencies and styles for Explore page

---
+++ src/client/componets/Poster.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Poster({ img }) {
+    const url = "https://image.tmdb.org/t/p/w500/" + img;
+    return (
+        <div className="poster">
+            <img src={`${url}`} alt="" />
+        </div>
+    );
+}
--- src/client/pages/Explore.jsx
+++ src/client/pages/Explore.jsx
@@ -1,5 +1,51 @@
 import React from "react";
-
+import "../styles/Explore.css";
+import Poster from "../componets/Poster";
+import { LeftCircleFilled, RightCircleFilled } from "@ant-design/icons";
 export default function Explore() {
-    return <div className="explore_screen">Explore</div>;
+    const onPressLeft = () => {
+        const cont1 = document.getElementById("scrollContainer");
+        cont1.scrollBy({ left: -300, behavior: "auto" });
+    };
+
+    const onPressRight = () => {
+        const cont1 = document.getElementById("scrollContainer");
+        cont1.scrollBy({ left: 420, behavior: "auto" });
+    };
+
+    return (
+        <div className="exploreScreen">
+            <div className="carouselContainer">1</div>
+            <div className="cardContainer">
+                <LeftCircleFilled
+                    className="scrollLeftBtn"
+                    onClick={onPressLeft}
+                />
+                <div className="scrollContainer" id="scrollContainer">
+                    <Poster />
+                    <Poster />
+                    <Poster />
+                    <Poster />
+                    <Poster />
+                    <Poster />
+                    <Poster />
+                    <Poster />
+                    <Poster />
+                    <Poster />
+                    <Poster />
+                    <Poster />
+                    <Poster />
+                    <Poster />
+                    <Poster />
+                    <Poster />
+                    <Poster />
+                    <Poster />
+                </div>
+                <RightCircleFilled
+                    className="scrollRightBtn"
+                    onClick={onPressRight}
+                />
+            </div>
+        </div>
+    );
 }
