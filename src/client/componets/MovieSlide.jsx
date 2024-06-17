4815c2b531af4817410cac7c88c27d8fd3f8c978
Lu <96496642+LuluDev1@users.noreply.github.com>
2024-06-17 16:27:08 -0500

    feat: Add trending and popular movie slides to Explore page
    This commit adds two new movie slides, "Trending" and "Popular", to the Explore page. The slides are implemented using the MovieSlide component and are fetched from the "/populartv" and "/trending" endpoints respectively. This enhances the functionality and design of the Explore page, providing users with more movie options to explore.

---
+++ src/client/componets/MovieSlide.jsx
@@ -0,0 +1,24 @@
+import { useState, useEffect } from "react";
+import Poster from "./Poster";
+
+export default function MovieSlide({ endpoint }) {
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        fetch(endpoint)
+            .then((response) => response.json())
+            .then((response) => setData(response.results))
+            .catch((err) => console.log(err));
+    }, []);
+    return (
+        <div className="slideContainer">
+            {data ? (
+                data.map((movie, index) => {
+                    return <Poster index={index} img={movie.poster_path} />;
+                })
+            ) : (
+                <p>Error</p>
+            )}
+        </div>
+    );
+}
--- src/client/pages/Explore.jsx
+++ src/client/pages/Explore.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 // STYLING
 import "../styles/Explore.css";
-
+import MovieSlide from "../componets/MovieSlide";
 // COMPONENTS
 import Carousel from "../componets/Carousel";
 import logo from "../assets/logo.png";
@@ -15,6 +15,14 @@ export default function Explore() {
                 <img src={logo} alt="" />
             </div>
             <Carousel />
+            <div className="slideTitle">
+                <h1>Trending</h1>
+            </div>
+            <MovieSlide endpoint="/populartv" />
+            <div className="slideTitle">
+                <h1>Popular</h1>
+            </div>
+            <MovieSlide endpoint="/trending" />
         </div>
     );
 }
